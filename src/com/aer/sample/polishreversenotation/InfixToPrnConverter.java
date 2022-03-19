fe0b61997ffa5ab53374c9b3e1f998756da433bc
aegido <alberto.egido@tui.com>
2022-03-19 01:12:20 +0100

    remove critical issue of complexity

--- src/com/aer/sample/polishreversenotation/InfixToPrnConverter.java
+++ src/com/aer/sample/polishreversenotation/InfixToPrnConverter.java
@@ -23,35 +23,37 @@ public class InfixToPrnConverter {
     Stack<String> operands = new Stack();
     for (int i = 0; i < trimmedToken.length(); i++) {
       String symbol = getString(trimmedToken, i);
-      if (isOperand(symbol)) {
-        while (!operands.isEmpty() && precedence.get(operands.peek()) >= precedence.get(symbol)) {
-          result.add(operands.pop());
-        }
-        operands.push(symbol);
-      } else if (symbol.equals("(")) {
-        operands.push(symbol);
-      } else if (symbol.equals(")")) {
-        String operand = operands.pop();
-        while (!operand.equals("(")) {
-          result.add(operand);
-          operand = operands.pop();
-        }
-      } else {
-        String number = symbol;
-        while (i + 1 < trimmedToken.length() && checkNumber(symbol, getString(trimmedToken, i + 1))) {
-          number += getString(trimmedToken, i+1);
-          i++;
-        }
-        result.add(number);
+      switch (symbol) {
+        case "+": case "-": case "/": case "*":
+          while (!operands.isEmpty() && precedence.get(operands.peek()) >= precedence.get(symbol)) {
+            result.add(operands.pop());
+          }
+          operands.push(symbol);
+          break;
+        case "(":
+          operands.push(symbol);
+          break;
+        case ")":
+          String operand = operands.pop();
+          while (!operand.equals("(")) {
+            result.add(operand);
+            operand = operands.pop();
+          }
+          break;
+        default:
+          String number = symbol;
+          while (i + 1 < trimmedToken.length() && checkNumber(symbol, getString(trimmedToken, i + 1))) {
+            number += getString(trimmedToken, i + 1);
+            i++;
+          }
+          result.add(number);
       }
-
     }
     addRemainingOperands(result, operands);
-    return result.toString().
-
-      replaceAll("\\[|\\]|\\s", "");
-
+    return result.toString().replaceAll("[\\[\\]\\s]", "");
   }
+
+
 
   private static String getString(String expression, int index) {
     return String.valueOf(expression.charAt(index));
@@ -65,11 +67,6 @@ public class InfixToPrnConverter {
     } catch (NumberFormatException exception) {
       return false;
     }
-  }
-
-  private static boolean isOperand(String symbol) {
-    List<String> operands = Arrays.asList("+", "-", "*", "/");
-    return operands.contains(symbol);
   }
 
   private static String removeWhiteSpace(String token) {
